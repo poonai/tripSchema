188526b2cfd4630d3ac78952e9c05a8177ab3c4a
root <rbalajis25@gmail.com>
2016-08-10 14:15:11 +0530

    extra removed

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/',require('./profile/index.js'));
 app.use('/location',require('./location'))
 app.use('/selfie',require('./selfie/index.js'))
-app.use('/uber',require('./uber/index.js'))
+//app.use('/uber',require('./uber/index.js'))
 app.use('/mileage',require('./mileage'))
 //app.use('/message',require('./mg91/index.js'))
-app.use('/twilio',require('./twilio/index.js'))
+//app.use('/twilio',require('./twilio/index.js'))
 app.use(require('./util/error.js').errorHandler);
 app.listen(process.env.PORT||1234)
