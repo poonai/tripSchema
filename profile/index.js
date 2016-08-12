5ea1666bb8420cbb92a8ac6e9d2b219b97cebbad
root <rbalajis25@gmail.com>
2016-08-12 23:08:07 +0530

    new change

--- profile/index.js
+++ profile/index.js
@@ -5,7 +5,12 @@ var model=require('./model.js')
 profile.post('/',require('body-parser').json(),function (req,res,next) {
 model.login(req,res,next);
 })
-
+profile.get('/',function (req,res,next) {
+  res.json({
+    status:true,
+    message:"working"
+  })
+})
 profile.post('/selfie',require('body-parser').json(),require('../util/auth.js').userPostAuth,function (req,res,next) {
 model.saveSelfie(req,res,next);
 })
